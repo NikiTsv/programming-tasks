49a4b153d7ed94d9bba18701dfc663048972397e
nikolay tsvetanov <bl4ze@abv.bg>
2019-06-06 18:40:58 +0300

    comments and renaming

--- Xara-BE/index.js
+++ Xara-BE/index.js
@@ -1,14 +1,41 @@
 const groupBy = require('./utility').groupBy;
 const getParentsFromGroupAndAddFirstChild = require('./utility').getParentsFromGroupAndAddFirstChild;
+const fs = require('fs');
+const dataPoints = require('./datapoints');
 
 const main = (data) => {
 
+    /*
+    { '8':
+    [ { start: 1526915703284, tier: '2467-359' },
+        { start: 1526915703274, tier: '2467-359' } ],
+    '13':
+    [ { start: 1526915704059, tier: '2467-359-5619' },
+        { start: 1526915704056, tier: '2467-359-5619' },
+        { start: 1526915704333, tier: '2467-359-8650' },
+        ... ],
+    '17':
+    ...
+    */
+    // grouped all objects by length of tier
     const tierMap = {};
+
+    /*
+        2':
+        [ { start: 1526915707425,
+            tier: '2467-359-963-6177',
+            children: [Array] },
+            { start: 1526915707793,
+            tier: '2467-359-963-7209',
+            children: [Array] } ],
+    */
+
+    // parents grouped by their tier value length
     const tierOrder = {};
     let maxTier = 0;
 
-    for (let i = 0; i < dataCopy.length; i++) {
-        const el = Object.assign({}, dataCopy[i]);
+    for (let i = 0; i < data.length; i++) {
+        const el = Object.assign({}, data[i]);
         const len = el.tier.length;
         if (!tierMap[len]) {
             tierMap[len] = [];
@@ -17,36 +44,40 @@ const main = (data) => {
         tierMap[len].push(el);
     }
 
-    //console.log('tiermap', tierMap);
-
     for (let prop in tierMap) {
-        const tier = tierMap[prop];
-        const group = groupBy(tier, 'tier');
-        const parentsInTier = getParentsFromGroupAndAddFirstChild(group);
+        const tierWithSameLength = tierMap[prop];
 
-        tierOrder[maxTier] = parentsInTier;
+        // group same tier so first child with later start can be assigned
+        const group = groupBy(tierWithSameLength, 'tier');
+
+        // assign child first child for each parent
+        const parentsInTierWithFirstChild = getParentsFromGroupAndAddFirstChild(group);
+
+        // grouping parents by their tier value length
+        tierOrder[maxTier] = parentsInTierWithFirstChild;
 
         maxTier++;
     }
 
-    //console.log('TO', tierOrder);
-
-    //for each tier starting from 15
+    // for each tier starting from the last one
+    // try to assign it as child to a lower tier
     for (let i = maxTier - 1; i >= 0; i--) {
 
         const currTier = tierOrder[i];
 
-        //for each lower tier starting from 14
+        // for each lower tier starting from current - 1
+        // try to assign children
+        // if assigned child value becomes null so it's not assigned anymore
         for (let j = i - 1; j >= 0; j--) {
-            const prevTier = tierOrder[j];
+            const lowerTier = tierOrder[j];
 
-            if (currTier && prevTier) {
-                for (let k = 0; k < prevTier.length; k++) {
-                    const pt = prevTier[k];
+            if (currTier && lowerTier) {
+                for (let k = 0; k < lowerTier.length; k++) {
+                    const ltk = lowerTier[k];
                     for (let x = 0; x < currTier.length; x++) {
-                        let ct = currTier[x];
-                        if (ct && pt && ct.tier != pt.tier && ct.tier.includes(pt.tier)) {
-                            pt.children.push(ct);
+                        let ctx = currTier[x];
+                        if (ctx && ltk && ctx.tier != ltk.tier && ctx.tier.includes(ltk.tier)) {
+                            ltk.children.push(ctx);
                             currTier[x] = null;
                         }
                     }
@@ -57,6 +88,15 @@ const main = (data) => {
 
     return tierOrder[0];
 }
+
+const result = main(dataPoints);
+
+fs.writeFile("result.txt", JSON.stringify(result, null, 4), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The result was saved to result.txt!");
+});
 
 module.exports = {
     main
--- Xara-BE/test.js
+++ Xara-BE/test.js
@@ -1,8 +1,8 @@
 const main = require('./index').main;
 const dataPoints = require('./datapoints');
+const expected = require('./expectedResult');
 
 let result = main(dataPoints);
-console.log('result1', result[0].children.length);
+console.log('result', result[0].children.length);
+console.log('expected result', expected[0].children.length);
 
-let result2 = main(dataPoints);
-console.log('result2', result2[0].children.length);
